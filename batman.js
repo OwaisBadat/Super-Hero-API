16a26035bda9dc18bd263236ed898a7941757f01
Owais Badat <owaisbadat@gmail.com>
2018-10-15 21:04:51 -0500

    first commit

--- batman.js
+++ batman.js
@@ -1,5 +1,5 @@
 
-const MOVIES_URL = "http://www.omdbapi.com/?s=batman&apikey=281fbaa7"
+const MOVIES_URL = "http://www.omdbapi.com/?s=batman&apikey=" + myKey
 
 
 let movieLinks = document.getElementById('movieLinks')
@@ -8,27 +8,18 @@ let movieInfo = document.getElementById('movieInfo')
 function displayMovies(movies) {
 
   let movieItems =  movies.Search.map(function(movie){
+      let movieID = movie.imdbID
       return `<li>
       <div><img src="${movie.Poster}"></img></div>
-      <div><a onclick="displayMovieInfo(${movie.imdbID})"href="#movieInfo">${movie.Title}</a></div>
+      <div><a onclick="displayMovieInfo('${movieID}')" href="#movieInfo">${movie.Title}</a></div>
       </li>`
 
 
   })
   movieLinks.innerHTML = movieItems.join('')
+
 }
 
-
-function displayMovieInfo(imdbID){
-  return `<li>
-  <div>
-    <title>${movie.Title}</title>
-    <img src="${movie.Poster}"></img>
-  </li>
-  <li>
-
-  </li>`
-}
 function getMovies() {
 
   $.get(MOVIES_URL,function(data){
